17c1b6df2f2cc36f34203d1fc60dd041e090e765
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-10-28 11:41:51 -0300

    feat: updating DataBaseCarrierAPI

---
+++ src/main/java/org/example/models/entity/DataBaseCarrierAPI.java
@@ -0,0 +1,44 @@
+package org.example.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "transportadora")
+@Data
+@NoArgsConstructor
+public class DataBaseCarrierAPI {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome_cliente", nullable = false)
+    private String nomeCliente;
+
+    @Column(name = "cpf", unique = true, length = 11)
+    private String cpf;
+
+    @Column(name = "status_pedido")
+    private String statusPedido;
+
+    @Column(name = "codigo_rastreio", unique = true)
+    private String codigoRastreio;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+}
