11b4b859c8e6c94402c5e93f0f1fc5fbde5bb0e5
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-10-30 13:06:01 -0300

    feat: add dependencies

--- src/main/java/org/example/service/CustomerService.java
+++ src/main/java/org/example/service/CustomerService.java
@@ -1,4 +1,28 @@
 package org.example.service;
 
+import org.example.entity.Customers;
+import org.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customers> getAll(){
+        return customerRepository.findAll();
+    }
+
+    public Customers getByCpf(String cpf) {
+        return customerRepository.findByCpf(cpf);
+    }
+
+    public Customers create(Customers customers) {
+        return customerRepository.save(customers);
+    }
+
 }
