9dd2f693ee9262b9bca321f6a4cf79d261e46f07
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-10-30 09:20:04 -0300

    feat: add Location

---
+++ src/main/java/org/example/client/mail/response/Location.java
@@ -0,0 +1,13 @@
+package org.example.client.mail.response;
+
+public class Location {
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
